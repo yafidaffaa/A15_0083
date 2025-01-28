7b73a945b0aa06cd4b796dd8c188ea98d8d8535a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-28 12:45:43 +0700

    membuat monitoring application untuk mengarahkan ke pengelola halaman

---
+++ app/src/main/java/com/yafidaffaaa/uas_pam/ui/viewmodel/MonitoringApplication.kt
@@ -0,0 +1,34 @@
+package com.yafidaffaaa.uas_pam.ui.viewmodel
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import com.yafidaffaaa.uas_pam.ui.navigation.PengelolaHalaman
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MonitoringApplication(
+    modifier: Modifier
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            PengelolaHalaman()
+        }
+    }
+}
