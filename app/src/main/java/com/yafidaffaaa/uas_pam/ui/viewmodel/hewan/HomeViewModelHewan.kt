f22226e6a636ab267b1bbcf7a2205c97e0b74cf6
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-28 12:11:34 +0700

    membuat fungsi untuk menampilkan semua hewan

---
+++ app/src/main/java/com/yafidaffaaa/uas_pam/ui/viewmodel/hewan/HomeViewModelHewan.kt
@@ -0,0 +1,48 @@
+package com.yafidaffaaa.uas_pam.ui.viewmodel.hewan
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.yafidaffaaa.uas_pam.model.Hewan
+import com.yafidaffaaa.uas_pam.repository.HewanRepository
+import kotlinx.coroutines.launch
+import okio.IOException
+
+sealed class HomeUiState {
+    data class Success(val hewan: List<Hewan>) : HomeUiState()
+    object Loading : HomeUiState()
+    data class Error(val message: String) : HomeUiState()
+}
+
+class HomeViewModelHewan(private val hwn: HewanRepository) : ViewModel() {
+    var hwnUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getHewan()
+    }
+
+    fun getHewan() {
+        viewModelScope.launch {
+            hwnUIState = HomeUiState.Loading
+            hwnUIState = try {
+                HomeUiState.Success(hwn.getHewan())
+            } catch (e: IOException) {
+                Log.e("HewanViewModel", "Kesalahan jaringan: ${e.message}", e)
+                HomeUiState.Error("Kesalahan jaringan: ${e.message}")
+            } catch (e: HttpException) {
+                Log.e("HewanViewModel", "Kesalahan server: ${e.message}", e)
+                HomeUiState.Error("Kesalahan server: ${e.message}")
+            } catch (e: Exception) {
+                Log.e("HewanViewModel", "Terjadi kesalahan tak terduga: ${e.message}", e)
+                HomeUiState.Error("Terjadi kesalahan tak terduga: ${e.message}")
+            }
+        }
+    }
+
+
+}
