26b4c72f7d402b85d4cb0854057883fb9d7a7a9f
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-28 12:26:12 +0700

    membuat data class insertuievent

---
+++ app/src/main/java/com/yafidaffaaa/uas_pam/ui/viewmodel/hewan/InsertViewModelHewan.kt
@@ -0,0 +1,21 @@
+package com.yafidaffaaa.uas_pam.ui.viewmodel.hewan
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yafidaffaaa.uas_pam.model.Hewan
+import com.yafidaffaaa.uas_pam.repository.HewanRepository
+import kotlinx.coroutines.launch
+
+
+
+data class InsertUiEvent(
+    val id_hewan: String = "",
+    val nama_hewan: String = "",
+    val populasi: String = "",
+    val tipe_pakan: String = "",
+    val zona_wilayah: String = "",
+)
