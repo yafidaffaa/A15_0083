3022071e582b7c794de412200202390cab13d040
yafidaffaa <yafi.d.andriansyah@gmail.com>
2025-01-28 11:35:42 +0700

    Initial commit

---
+++ app/src/main/java/com/yafidaffaaa/uas_pam/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.yafidaffaaa.uas_pam
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yafidaffaaa.uas_pam.ui.navigation.PengelolaHalaman
+import com.yafidaffaaa.uas_pam.ui.theme.UAS_PAMTheme
+import com.yafidaffaaa.uas_pam.ui.viewmodel.MonitoringApplication
+
+class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UAS_PAMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MonitoringApplication(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UAS_PAMTheme {
+        Greeting("Android")
+    }
+}
